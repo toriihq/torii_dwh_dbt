503b8c96fe4ee177085cc4e8748c77d9c3d4db54
udilerner <udi.lerner@toriihq.com>
2022-07-11 09:02:55 +0300

    updated analyst init script

--- db_scripts/roles_init/analyst_init.sql
+++ db_scripts/roles_init/analyst_init.sql
@@ -29,8 +29,10 @@ begin;
     grant usage on DATABASE torii_dwh_dev to role identifier($role_name);
     grant usage on schema torii_dwh_dev.DATA_MART_DEV to role identifier($role_name);
     grant select on all tables in schema torii_dwh_dev.DATA_MART_DEV to role identifier($role_name);
+    grant select on all views in schema torii_dwh_dev.DATA_MART_DEV to role identifier($role_name);
     -- grant on future table
     use role ACCOUNTADMIN;
     grant select on future tables in schema torii_dwh_dev.DATA_MART_DEV to role identifier($role_name);
+    grant select on future views in schema torii_dwh_dev.DATA_MART_DEV to role identifier($role_name);
 
 commit;
