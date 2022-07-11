182e91f31d59e71830269d1339008ec02573587f
udilerner <udi.lerner@toriihq.com>
2022-07-11 08:42:27 +0300

    updated db init script

--- db_scripts/db_init.sql
+++ db_scripts/db_init.sql
@@ -1,31 +1,21 @@
 use role SYSADMIN;
 
 -- init dev databases
-create database data_marts_dev;
-create  database data_vault_dev;
-create  database stage_dev;
+create database torii_dwh_dev;
 
 -- init prod databases
-create database data_marts;
-create database data_vault;
-create database stage;
+create database torii_dwh;
 
 use role SYSADMIN;
 -- init dev schema
-create schema data_marts_dev.mart_global_dev;
-create schema data_marts_dev.mart_action_audit_dev;
+create schema torii_dwh_dev.raw_stage_dev;
+create schema torii_dwh_dev.stage_dev;
+create schema torii_dwh_dev.data_vault_dev;
+create schema torii_dwh_dev.data_mart_dev;
 
-create schema data_vault_dev.raw_vault_dev;
-create schema data_vault_dev.biz_dev;
-
-create schema stage_dev.raw_stage_dev;
-create schema stage_dev.stage_dev;
 
 -- init prod schema
-create schema data_marts.mart_global;
-create schema data_marts.mart_action_audit;
-
-create schema data_vault.raw_vault;
-create schema data_vault.biz;
-
-create schema stage.raw_stage;
+create schema torii_dwh.raw_stage;
+create schema torii_dwh.stage;
+create schema torii_dwh.data_vault;
+create schema torii_dwh.data_mart;
